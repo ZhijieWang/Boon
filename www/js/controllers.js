af9e3899d43bcb47b3e68e7af8212eb6e81500ac
Adnan <adnankhan@gmail.com>
2015-01-26 21:31:40 -0500

    Switched to rebuilt Ionic code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,45 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('DealsCtrl', function($scope, Deals, $ionicSlideBoxDelegate ) {
+.controller('MainCtrl', function($scope) {})
+.controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
 
-   $scope.deals = Deals.all();
-   $ionicSlideBoxDelegate.update();
+        // Opens the left menu when left button is hit
+        $scope.showMenu = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
 
-   // Swipe Handler
-   $scope.slideHasChanged = function(index) {
+        // Opens the right menu when right button is hit
+        $scope.showRightMenu = function () {
+            $ionicSideMenuDelegate.toggleRight();
+        };
+    })
+.controller('DealsCtrl',[ '$scope','$ionicSlideBoxDelegate','Deals', function($scope, $ionicSlideBoxDelegate ,Deals) {
+        // Controls deals that user has viewed and their selection
+        // state of those deals ( user's reaction to deal, how long
+        // they spent looking at the deal )
 
-   }
-})
+         $scope.deals = Deals.all();
 
-.controller('DealDetailCtrl', function($scope, $stateParams, Deals) {
-  $scope.deal = Deals.get($stateParams.dealId);
-})
+        $ionicSlideBoxDelegate.update();
+
+        // Rejects the deal and removes from user's possible list of deals for the day
+        $scope.leftSwipe = function(currentDeal) {
+
+        }
+
+        // Adds deal to stash and logs that user accepted the deal
+        $scope.rightSwipe = function(currentDeal) {
+
+        }
+    }])
+.controller('PriceCtrl', function($scope) {
+
+        // Holds user's selections for price ranges
+        $scope.devList = [
+            { text: " 0 - 5 $ ", checked: false },
+            { text: " 5 - 10 $$ ", checked: false },
+            { text: " 10-20 $$$ ", checked: false }
+        ];
+
+    })
--- www/js/services.js
+++ www/js/services.js
@@ -1,65 +1,59 @@
+/**
+ * Created by adnankhan on 1/24/15.
+ *
+ *
+ *      Initially we have mocked deal data for testing, but we will
+ *      receive JSON deal data from our backend to populate deals with
+ *
+ */
+
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Deals', function() {
-  // Might use a resource here that returns a JSON array
-  // Swipe State Options:
-  // 0 - unswiped
-  // 1 - swiped right
-  // 2 - swiped left
+    .factory('Deals', function() {
+        // Might use a resource here that returns a JSON array
 
-  // Fake Deals for Demo
-  var deals = [{
-    id: 0,
-    name: 'Hemingway\'s Half Off Food',
-    notes: 'Voted best college bar in Oakland!',
-    displayImage: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',
-    startTime: 0500,
-    endTime: 0700,
-    swipeState: 0
-  }, {
-    id: 1,
-    name: 'Peter\'s Pub',
-    notes: 'Odd obsession with everything',
-    displayImage: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460',
-    startTime: 0500,
-    endTime: 0700,
-    swipeState: 0
-  }, {
-    id: 2,
-    name: 'Hemingway\'s Free Drinks',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    displayImage: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg',
-    startTime: 0500,
-    endTime: 0700,
-    swipeState: 0
-  }, {
-    id: 3,
-    name: 'Five Guys Bacon Dog',
-    notes: 'Can\'t go wrong with bacon!',
-    displayImage: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg',
-    startTime: 0500,
-    endTime: 0700,
-    swipeState: 0
-  }, {
-    id: 4,
-    name: 'Panera Free Drink With Purchase!',
-    notes: 'Freshly baked bread daily!',
-    displayImage: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg',
-    startTime: 0500,
-    endTime: 0700,
-    swipeState: 0
-  }];
+        // Fake Deals for Testing
+        var deals = [{
+            id: 0,
+            name: 'Hemingway\'s Half Off Food',
+            notes: 'Voted best college bar in Oakland!',
+            startTime: 0500,
+            endTime: 0700
 
-  return {
-    all: function() {
-      return deals;
-    },
-    get: function(dealId) {
-      // Simple index lookup
-      return deals[dealId];
-    }
-  }
-});
+        }, {
+            id: 1,
+            name: 'Peter\'s Pub',
+            notes: 'Odd obsession with everything',
+            startTime: 0500,
+            endTime: 0700
+
+        }, {
+            id: 2,
+            name: 'Hemingway\'s Free Drinks',
+            notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
+            startTime: 0500,
+            endTime: 0700
+        }, {
+            id: 3,
+            name: 'Five Guys Bacon Dog',
+            notes: 'Can\'t go wrong with bacon!',
+            startTime: 0500,
+            endTime: 0700
+        }, {
+            id: 4,
+            name: 'Panera Free Drink With Purchase!',
+            notes: 'Freshly baked bread daily!',
+            startTime: 0500,
+            endTime: 0700
+        }];
+
+        return {
+            all: function() {
+                return deals;
+            },
+            get: function(dealId) {
+                // Simple index lookup
+                return deals[dealId];
+            }
+        }
+    })
