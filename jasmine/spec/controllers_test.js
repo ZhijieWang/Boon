876a2a2c4bb167dae56f805c6fdf791b86e9f3cf
Tyler Raborn <tpr11@pitt.edu>
2015-01-31 14:24:03 -0500

    fixed test

--- jasmine/spec/controllers_test.js
+++ jasmine/spec/controllers_test.js
@@ -7,6 +7,7 @@ describe('controllers module', function() {
     beforeEach(module('starter'));
 
     var dealsService;
+    var dealCacheService;
     var getController;
 
     var mockDeal = {
@@ -18,7 +19,7 @@ describe('controllers module', function() {
         endTime: 0700
     };    
 
-    beforeEach(inject(function($rootScope, $controller, $injector, _dealsService_) {
+    beforeEach(inject(function($rootScope, $controller, $injector, _dealsService_, _dealCacheService_) {
         
         // use injector to grab default services
         $httpBackend = $injector.get('$httpBackend');
@@ -32,19 +33,20 @@ describe('controllers module', function() {
         // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
         // This allows us to inject a service but then attach it to a variable
         // with the same name as the service.
+        dealCacheService = _dealCacheService_;
         dealsService = _dealsService_;   
     }));
 
     it ('should add an accepted deal to the services list of accepted deals', function() {
         var theController = getController('DealsCtrl');
         scope.cardSwipedRight(mockDeal);
-        expect(scope.stashedDeals).toContain(mockDeal);
+        expect(dealCacheService.stashedDeals()).toContain(mockDeal);
     });
 
     it ('should add a rejected deal to the services master list of rejected deals', function() {
         var theController = getController('DealsCtrl');
         scope.cardSwipedLeft(mockDeal);
-        expect(scope.rejectedDeals).toContain(mockDeal);
+        expect(dealCacheService.stashedDeals).not.toContain(mockDeal);
     }); 
 
 
