c883c52ae86ec15d968d615a2096ab3bbf60cc08
Adnan <adnankhan@gmail.com>
2015-02-16 13:31:26 -0500

    removed uneeded state

--- www/js/app.js
+++ www/js/app.js
@@ -74,15 +74,6 @@ angular.module('starter', ['ionic','ionic.contrib.ui.tinderCards','starter.servi
                   }
               }
             })
-            .state('tab.deal-detail', {
-                url: '/deal-detail',
-                views: {
-                    'deal-detail': {
-                        templateUrl: 'templates/deal-detail.html',
-                        controller: 'DealsCtrl'
-                    }
-                }
-            })
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/splash');
 
